c678a08bf4164dffaf1e8ec52d2dd8394ba2c2d6
angelicafranca94 <gefranca94@gmail.com>
2022-04-20 10:39:33 -0300

    Criando a DbContext

--- PlatformService/PlatformService/Program.cs
+++ PlatformService/PlatformService/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Dependence Injection
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseInMemoryDatabase("InMem"));
+
+#endregion
 
 var app = builder.Build();
 
